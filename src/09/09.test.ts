dc98a41e95d2a090a074d4c648b576e0e26d3a1c
sandvik16 <sannndvik16@gmail.com>
2021-08-16 22:21:16 +0300

    mutable and imutable

---
+++ src/09/09.test.ts
@@ -0,0 +1,70 @@
+function increaseAge(u: UserType) {
+    u.age++;
+}
+
+type UserType = {
+    name: string
+    age: number
+}
+
+test('reference type test', () => {
+
+
+    const user = {
+        name: 'Andrew',
+        age: 31
+    }
+
+    increaseAge(user);
+
+    expect(user.age).toBe(32)
+
+    const superman = user;
+
+    superman.name = 'Andreyka'
+
+    expect(user.name).toBe('Andreyka')
+
+});
+
+test('array reference test', () => {
+    const users = [
+        {
+            name: 'Andrew',
+            age: 31,
+        },
+        {
+            name: 'Ira',
+            age: 33
+        }
+    ]
+
+    const usersCount = 100;
+
+    const adminsCount = usersCount;
+
+    const admins = users;
+    admins.push({name: 'Bandygan', age: 10})
+
+    expect(users[2]).toEqual({name: 'Bandygan', age: 10})
+})
+
+test('array reference test', () => {
+    const user = {
+        name: 'Andrew',
+        age: 31,
+        address: {
+            title: 'Grodno'
+        }
+    }
+
+    let addr = user.address;
+
+    const user2 = {
+        name: 'Petya',
+        age: 30,
+        address: addr
+    }
+
+    expect(user2.address.title).toBe('Grodno')
+})
